b6fa35d4da27ee715a2cd0c0f462f15889143590
Banja David Lotigo <innocdavid@gmail.com>
2024-07-08 11:43:26 +0300

    refactor algorigthm to a better time complexity

--- 001.js
+++ 001.js
@@ -1,13 +1,26 @@
-function twoSums (nums, target) {
-    let pairs = [];
-    for (let i=0; i<nums.length; i++) {
-        for (let j=i+1; j<nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-               pairs.push([i, j]);
-            }
+// function twoSums (nums, target) {
+//     let pairs = [];
+//     for (let i=0; i<nums.length; i++) {
+//         for (let j=i+1; j<nums.length; j++) {
+//             if (nums[i] + nums[j] === target) {
+//                pairs.push([i, j]);
+//             }
+//         }
+//     }
+//     return pairs; 
+// }
+
+function twoSums(nums, target) {
+    let seen = {};
+    pair = [];
+    for (let ind=0; ind<nums.length; ind++) {
+        let num = nums[ind];
+        if ((target - num) in seen) {
+            pair.push([ind, seen[target - num]]);
         }
+        seen[num] = ind;
     }
-    return pairs; 
+    return pair;
 }
 
 const nums = [1, 2, 3, 4, 5, 6, 7];
